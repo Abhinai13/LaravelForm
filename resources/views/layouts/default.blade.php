492bd38620f91b33973491a8a5085d2cef6057df
Abhinai <AP565@NJIT.EDU>
2019-04-14 11:34:06 -0400

    jumbotron setup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,13 @@
 @include('includes.head')
 
 <body>
-@yield('content')
+    @include('includes.menu')
+    <main role="main">
+        @yield('content')
+    </main>
 
 @include('includes.jsfooter')
+    @include('includes.footer')
 
 </body>
 </html>
