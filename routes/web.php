89879d466462b567617d39cc63fbb029412b4d8b
Abhinai <AP565@NJIT.EDU>
2019-04-14 19:46:35 -0400

    small update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+
+
+Route::get('/', 'PagesController@index')->name('index');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,15 +15,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
-Route::get('/', 'PagesController@home')->name('home');
-Route::get('/about', 'PagesController@about')->name('about');
-Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', function () {
     return view('contact');
     $data = request()->all();
     echo "Email: " . $data['email'] . '<br>';
     echo "Body: " . $data['body'];
-
 });
+*/
+
